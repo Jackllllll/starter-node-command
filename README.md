2a45930270fda89ab6693956308efb6d37d4fd7e
linzb <lin302010@qq.com>
2022-08-12 17:06:07 +0800

    chore: init

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 ## a node command template
+----------
+### install 
+```bash
+pnpm i
+
+```
+----------
+
+### 开发
+
+```bash
+pnpm dev
+```
+----------
+
+### 构建
+
+```bash
+pnpm build
+```
+
+### 运行
+
+```bash
+// 创建全局包的链接
+pnpm link --global 
+// 执行包的命令
+starter-node-command  -v or starter-node-command 
+
+```
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-function getSayello(str:string) : string{
-    return 'Hello Wolld';
-}
+import {argv, exit} from 'process';
+
+
+(function start() {
+    const args2 = argv.slice(2)[0];
+
+
+    if (args2 && args2 === '-v') {
+        const versio = require('../package.json').version;
+        // 输出版本号
+        console.log(versio);
+        exit(1);
+    }
+
+    function getSayello(): string {
+        return 'Hello Wolld';
+    }
+    // Hello Wolld
+    console.log(getSayello());
+})();
+
